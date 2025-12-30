f12799a3a748700fd2800ef8a91d619c04a36b4b
Willian-Thrd <business.will-theo@outlook.com>
2025-12-30 14:47:28 -0300

    Comecei a fazer a tabela para delivery

--- src/main/java/org/will/MainController.java
+++ src/main/java/org/will/MainController.java
@@ -1,6 +1,7 @@
 package org.will;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 
@@ -9,15 +10,20 @@ public class MainController {
     private ToggleButton delivery;
     @FXML
     private AnchorPane deliveryPane;
+    @FXML
+    private ComboBox<String> box;
+
 
     @FXML
     private void initialize() {
+        box.getItems().addAll("Débito", "Crédito", "Pix");
+
         delivery.selectedProperty().addListener((obs, oldVal, isOn) -> {
             if (isOn) {
-                delivery.setStyle("-fx-background-color: rgb(156, 156, 156); -fx-text-fill: green;");
+                delivery.setStyle("-fx-background-color: black; -fx-text-fill: lime;");
                 deliveryPane.setStyle("-fx-background-color: black; -fx-border-width: 2px; -fx-border-color: white;");
             } else {
-                delivery.setStyle("-fx-background-color: rgb(23, 23, 23); -fx-text-fill: green;");
+                delivery.setStyle("-fx-background-color: black; -fx-text-fill: red;");
                 deliveryPane.setStyle("-fx-background-color: black; -fx-border-width: 2px; -fx-border-color: white;");
             }
         });
